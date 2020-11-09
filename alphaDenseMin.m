c2ee4a2140d38a0a02398ff11296c6ba28431597
Matthew-Saurette <matthews14@hotmail.ca>
2020-11-09 13:47:18 -0800

    cleaned up some of outline

--- alphaDenseMin.m
+++ alphaDenseMin.m
@@ -1,24 +1,31 @@
 function [fbest] = alphaDenseMin(f,xbest,epsilon)
-%Calculate alpha and pick value n for alpha curve
+%%Calculate alpha and pick value n for alpha curve
 alpha = epsilon/2;
 n = 2/epsilon;
-x = spaceFillingFun(n)
-%initial conditions
-P = [] %Poll set
-xkbest = f(x(P(1)))
+
+
+%%define the space filling curve
+x = spaceFillingFun(n); 
+
+
+%%initial conditions
+P = []; %Poll set
+xkbest = f(x(P(1)));
+
+
 while norm(xkbest - xbest) >= epsilon %stopping condition
     
     %%Calculate convex evelope of spaceFillingFunction
     
-    %%%%%%%%%%%%
     
-    %Calculate Argmin convex evelope of spaceFillingFunction
     
-    %%%%%
+    %%Calculate Argmin convex evelope of spaceFillingFunction
     
-    %Expand the poll set
     
-    %%%%%
+    
+    %%Expand the poll set
+    
+    
     
     
 end
