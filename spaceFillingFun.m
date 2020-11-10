2347199d740ab8954171d04454e9334edabf21e4
Matthew-Saurette <matthews14@hotmail.ca>
2020-11-10 13:58:22 -0800

    implemented expanding poll set code

--- spaceFillingFun.m
+++ spaceFillingFun.m
@@ -2,13 +2,13 @@ function [x] = spaceFillingFun(n)
 
 
 %%calculate x1(t)
-x1 = @(t) t
+x1 = @(t) t;
 
 
 %%calculate x2(t)
-x2 = @(t) t
+x2 = @(t) t;
 %
-x = @(t)[x1(t),x2(t)]
+x = @(t)[x1(t),x2(t)];
 
 
 end
