9f88cdb3cff1853dc34d6cd234b9bbf74293e4d1
Matthew-Saurette <matthews14@hotmail.ca>
2020-11-11 22:21:09 -0800

    cleaned up some of the worstcase test

--- alphaDenseMin_Test.m
+++ alphaDenseMin_Test.m
@@ -5,9 +5,10 @@ end
 
 
 function test(testCase)
-k=9
+k=8
 epsilon = 1/(2^k);
-xmin = 0.5 + epsilon/2
+
+xmin = epsilon + epsilon/2
 ymin = 0.5-epsilon 
 f = @(x) (x(1)-(xmin))^2 + (x(2)-ymin)^2;
 xbest = [(xmin), ymin];
@@ -16,20 +17,4 @@ result = alphaDenseMin(f,xbest,epsilon)
 
 verifyEqual(testCase, result, xbest, 'AbsTol', epsilon); 
 end
-%function test2(testCase)
-%k=0;
-%worstindex = -1;
-%for i = 0.4:0.1:1
-    
-%f = @(x) (x(1)-(0.50005))^2 + (x(2)-i)^2;
-%xbest = [(0.50005), i];
-%epsilon = 0.0001;
-%result = alphaDenseMin(f,xbest,epsilon);
-%if result > k
-%    worstindex = i
-%    k = result
-%end
-%end
-%
-%verifyEqual(testCase, result, xbest, 'AbsTol', epsilon); 
-%end
+
