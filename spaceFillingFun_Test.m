e2a56a0c6ddbf9b8d1432c70aed41da164283a86
Matthew-Saurette <matthews14@hotmail.ca>
2020-11-25 13:59:09 -0800

    all changes made in lab nov 25

--- spaceFillingFun_Test.m
+++ spaceFillingFun_Test.m
@@ -5,9 +5,9 @@ end
 
 
 function test(testCase)
-x = spaceFillingFun(1)
+x = spaceFillingFun(0,3)
 result = x(1)
-expected = [1,1]
+expected = [1,0,0]
 
 verifyEqual(testCase, result, expected); 
 end
