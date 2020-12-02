4a9e6773db730ffd11d0142dcec4bed36fa8b48f
Jaden Balogh <jadenbalogh@gmail.com>
2020-12-01 21:35:29 -0800

    Update GridSearch and add tests

--- GridSearch.m
+++ GridSearch.m
@@ -1,46 +1,39 @@
 function [minloc] = GridSearch(f,xbest,epsilon)
+    % initial conditions
+    k = 1;
+    minloc = [0,0];
+    totalEvals = 0;
 
-
-%%initial conditions
-k=1 ;
-intervalPoints = 1/k;
-P = [];
-for i = 0:k
-    P = [P,i*intervalPoints];
-end
-minloc = [P(1),P(1)];
-
-while norm(minloc' - xbest) >= epsilon %stopping condition
-    
-    %%Expand the poll set
-    k = k*2;
-    intervalPoints = 1/k;
-    P = [];
-    for i = 0:k
-    P = [P,i*intervalPoints];
-    end
-    X =[];
-    for j = 1:length(P)
-        for i = 1:length(P)
-            X=[X,[P(j);P(i)]];
+    % continue checking more points until we are epsilon-close
+    while norm(minloc - xbest) >= epsilon
+        % define poll set data
+        interval = 1/k;
+        P = [];
+        for i = 0:k
+            P = [P,i*interval];
         end
-        
-    end
-    min = inf;
-    minloc = [];
-    for i = 1:length(X)
-        tempmin = f(X(:,i));
-        if tempmin < min
-            min  = tempmin;
-            minloc = X(:,i);
-        end
-    end
-    
-        
-        
-        
 
-    
-end
-disp(length(X(1,:)))
+        % store array of polling points
+        X = [];
+        for j = 1:length(P)
+            for i = 1:length(P)
+                X = [X;[P(j),P(i)]];
+            end
+        end
+        totalEvals = totalEvals + length(X);
+        
+        % get the lowest function value in poll set
+        min = inf;
+        minloc = [0,0];
+        for i = 1:length(X)
+            tempmin = f(X(i,:));
+            if tempmin < min
+                min  = tempmin;
+                minloc = X(i,:);
+            end
+        end
+
+        % expand the poll set
+        k = k*2;
+    end
 end
